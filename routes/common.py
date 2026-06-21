405c78f9bddf64af431c4b5747ea7fa07543efea
Илья Мышьяков <ilyamyshy228@gmail.com>
2026-06-21 20:34:07 +0500

    Added an endpoint for soft-deletion of profile. Also implemented permission checks

--- routes/common.py
+++ routes/common.py
@@ -24,7 +24,7 @@ async def mock_view():
     return {'message': 'OK'}
 
 @common_router.get('/my/profile', response_model=ReadProfile, tags=['My'])
-async def get_profile(session: AsyncSession = Depends(get_session), user = Depends(get_current_user)):
+async def get_profile(session: AsyncSession = Depends(get_session), user = Depends(can_read_own_profile)):
     result = await session.execute(
         select(User).where(User.user_id == user.user_id)
     )
@@ -32,5 +32,11 @@ async def get_profile(session: AsyncSession = Depends(get_session), user = Depen
     return profile
 
 @common_router.patch('/my/profile', response_model=ReadProfile, tags=['My'])
-async def patch_profile(schema: PatchProfile, session: AsyncSession = Depends(get_session), user = Depends(get_current_user)):
+async def patch_profile(schema: PatchProfile, session: AsyncSession = Depends(get_session), user = Depends(can_update_own_profile)):
     return await patch_record(id=user.user_id, model=User, schema=schema, session=session)
+
+@common_router.delete('/my/profile', tags=['My'])
+async def delete_profile(session: AsyncSession = Depends(get_session), user = Depends(can_delete_own_profile)):
+    user.is_active = False
+    await session.commit()
+    return {'message': 'Account was successfully deleted'}
